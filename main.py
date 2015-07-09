40694f14b5b5d61059f180da1b8bbb910d6d21b5
Chenguang Niu <chrisniu1984@gmail.com>
2015-07-09 15:53:45 +0800

    support display version by file

--- main.py
+++ main.py
@@ -4,8 +4,20 @@
 import os
 from NIU import Frame
 
+def get_ver(cwd):
+    verfile = cwd + "/version"
+    if os.path.exists(verfile) == False:
+        return ""
+
+    f = open(verfile)
+    ver = f.read()
+    f.close()
+    return ver.strip()
+
 if __name__ == "__main__":
     cwd = os.path.abspath(os.path.dirname(__file__))
-    frame = Frame("Juniu Connect Manager", "1.2", cwd)
+    ver = get_ver(cwd)
+
+    frame = Frame("Juniu Connect Manager", ver, cwd)
     frame.load_plugins()
     frame.loop()
